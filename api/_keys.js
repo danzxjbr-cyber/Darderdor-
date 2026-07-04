76b7e84f07c84886e6670b32cd7903a6e95c5cc9
danzxjbr-cyber <danzxjbr@gmail.com>
2026-07-04 19:34:09 +0800

    Create _keys.js

---
+++ api/_keys.js
@@ -0,0 +1,23 @@
+export const KEYS = {
+  pakasir: {
+    project: "WilzzOrder",
+    apiKey: "QSl1iHQodFLg7Bc3gjsROfvJxgWY3UQl",
+    baseUrl: "https://app.pakasir.com/api",
+  },
+
+  pterodactyl: {
+    domain: "https://",
+    apiKey: "ntar",
+    clientKey: "ptlc_VGN3HVCT6G9FuUoKkkmWdSl5j6pKLCXye6pYpFKHbfY",
+    egg: 15,
+    nestId: 5,
+    locationId: 1,
+  },
+
+  digitalocean: {
+    apiKey: "",
+    region: "sgp1",
+    image: "ubuntu-24-04-x64",
+  },
+};
+    
